35c358794c916d1d5773083a5574da907efb8d36
srinivas681 <srinivasteja681@gmail.com>
2021-08-05 18:44:56 +0530

    firstcommit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const text =[
+'goodmoreningshjdvfuhsvf ihsbvfihfbsriyfb  igyifbiwrfgi',
+`hjuytrd dtgh ergn dfvbnm dertyhjk dertghjm`,
+`erthjmmnbdsertghn vfghjk tyjk fgfhhjkl`,
+`dfbnmeodfikjvhbvhncxm, gfhdbnx tyrhdjxm ghfcnmxghfnc ghfjncm`,
+`wedfvbn dfgbnm tdcv lkjh dfgbnm `,
+`rghjk rthjk cfghnm`,
+`bjidbrigb ijdbrirgbiuebg`,
+` fgdvgdvf bfb ur  rhfbsifbeufyu hdbgiebf`,
+]
+
+export default text
